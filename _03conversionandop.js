93c4ccbc68a19d0cef813a9e17d06cc4dd08b6d1
sonamdev <dangisonam80@gmail.com>
2024-01-03 15:58:58 +0530

    T3 :type conversion

---
+++ _03conversionandop.js
@@ -0,0 +1,7 @@
+let score="33"
+//const {score}=req.body 
+console.log(typeof score)
+console.log(typeof (score))// in form of method
+let value= Number(score)// capital-coz its class based
+console.log(score)
+console.log(typeof value)
