f8a784b8356c11cbc652ba5e862fb59402a47a1c
sonamdev <dangisonam80@gmail.com>
2024-01-03 16:16:03 +0530

    T3.1-conversion

--- _03conversionandop.js
+++ _03conversionandop.js
@@ -1,7 +1,25 @@
-let score="33"
+//let score="33abc"
+//let score= null // get 0 after conversion
+//let score=undefined//get NaN
+let score= true // convert to 1
 //const {score}=req.body 
 console.log(typeof score)
 console.log(typeof (score))// in form of method
 let value= Number(score)// capital-coz its class based
 console.log(score)
-console.log(typeof value)
+console.log(typeof value)// check
+console.log(value)//NaN
+/*
+
+
+*/
+//let isloggedin=1
+//let isloggedin=""//empty string is considered false
+let isloggedin="sonam"//filled string is true
+let boolaenIsL=Boolean(isloggedin)
+console.log(boolaenIsL)
+console.log(typeof boolaenIsL)
+let somenum=33
+let snum= String(somenum)
+console.log(snum)
+console.log(typeof snum)
