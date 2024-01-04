811b4ef72b5e7d1b2abd696e817a7c7cf68155bd
sonamdev <dangisonam80@gmail.com>
2024-01-04 16:30:04 +0530

    operations

---
+++ _04operations.js
@@ -0,0 +1,19 @@
+let value=3
+let negvalue=-value
+//console.log(negvalue)
+//console.log(2+2)
+str1="hello"
+str2=" sonam"
+let str3=str1+str2
+//console.log(str3)
+console.log("1"+2)
+console.log(1+ "2")
+console.log("1" + "2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)// convert to number
+console.log(true)//normal true
+console.log(+"")// 1 
+let gamecounter=100
+gamecounter ++//read postfix and prefix-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+console.log(gamecounter)
